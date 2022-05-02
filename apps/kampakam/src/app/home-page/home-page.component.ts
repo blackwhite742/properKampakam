3f107515d523f9642500d09ed0ff721ac4e6f8ad
blackwhite742 <miha.petek21@outlook.com>
2022-05-02 16:04:59 +0200

    landing page generator form

--- apps/kampakam/src/app/home-page/home-page.component.ts
+++ apps/kampakam/src/app/home-page/home-page.component.ts
@@ -2,6 +2,21 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MegaMenuItem } from 'primeng/api/megamenuitem';
 import { MenuItem } from 'primeng/api/menuitem';
+import { MUNICIPALITIES } from '../../assets/municipalities2';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+const GENERATOR_FORM_FIELDS = {
+  regions: [''],
+  price:[''],
+  accessibility:[''],
+  accommodation:[''],
+  categories:[''],
+  season:[''],
+  name:[''],
+}
+
 
 @Component({
   selector: 'kampakam-home-page',
@@ -9,26 +24,50 @@ import { MenuItem } from 'primeng/api/menuitem';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent implements OnInit {
-  constructor(private router: Router) {}
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private http:HttpClient
+  ) {}
 
   items: MegaMenuItem[];
+  seasons:any[];
 
-  ngOnInit() {
-    this.items = [
-      {
-        label: 'Dodaj v DB',
-        icon: 'pi pi-fw pi-server',
-        routerLink: ['/addToDb'],
-      },
-      {
-        label: 'Random entry',
-        icon: 'pi pi-fw pi-download',
-        routerLink: ['/entry'],
-      },
-      {
-        label: 'Dogodki',
-        icon: 'pi pi-fw pi-calendar',
-      },
-    ];
+  //Categories
+  munOptions:any;
+  categoryOptions:any;
+  form: FormGroup;
+
+
+  //Ng Models
+  regions:any[];
+  priceChecked:boolean;
+  accessibilityChecked:boolean;
+  accommodationChecked:boolean;
+  selectedCategories:any[];
+  selectedSeason:string;
+
+  async ngOnInit() {
+    this.munOptions = MUNICIPALITIES;
+    this.seasons = [
+      {name:"Celoletno"},
+      {name:"Poletje"},
+      {name:"Zima"},
+    ]
+
+    this.form = this.formBuilder.group(GENERATOR_FORM_FIELDS);
+
+    this.categoryOptions = await firstValueFrom(
+      this.http.get(`/api/category/getAll`)
+    );
   }
+
+  submit(){
+    const formData=this.form.getRawValue();
+    console.log("Form data:",formData);
+
+  }
+
+
 }
--- apps/kampakam/src/app/home-page/home-page.module.ts
+++ apps/kampakam/src/app/home-page/home-page.module.ts
@@ -3,13 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { HomePageComponent } from './home-page.component';
 import {MegaMenuModule} from 'primeng/megamenu';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import {TreeSelectModule} from 'primeng/treeselect';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import {DropdownModule} from 'primeng/dropdown';
+import {InputTextModule} from 'primeng/inputtext';
 
 @NgModule({
   declarations: [HomePageComponent],
   imports: [
     CommonModule,
     ButtonModule,
-    MegaMenuModule
+    FormsModule,
+    ReactiveFormsModule,
+    MegaMenuModule,
+    MultiSelectModule,
+    TreeSelectModule,
+    InputSwitchModule,
+    DropdownModule
   ]
 })
 export class HomePageModule { }
