ff07fcc3fea73666983140a6d660669e12f36309
jureYGR <100045612+jureYGR@users.noreply.github.com>
2022-05-27 00:04:01 +0200

    mapa

--- apps/kampakam/src/app/mapa/mapa.component.ts
+++ apps/kampakam/src/app/mapa/mapa.component.ts
@@ -3,25 +3,22 @@ import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 @Component({
   selector: 'kampakam-mapa',
   templateUrl: './mapa.component.html',
-  styleUrls: ['./mapa.component.scss']
+  styleUrls: ['./mapa.component.scss'],
 })
 export class MapaComponent implements OnInit {
+  @Input() displayNavbar = true;
+  @Output() regionChange = new EventEmitter<string | null>();
 
-  @Input() displayNavbar=true;
-  @Output() regionChange=new EventEmitter<string|null>();
+  chosenRegion: string | null;
 
-  chosenRegion:string|null;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-  }
-
-  regionClick(data:string){
-    if(this.chosenRegion==data){
-      this.chosenRegion=null;
-    }
-    else{
+  regionClick(data: string) {
+    if (this.chosenRegion == data) {
+      this.chosenRegion = null;
+    } else {
       this.chosenRegion = data;
     }
     this.regionChange.emit(this.chosenRegion);
