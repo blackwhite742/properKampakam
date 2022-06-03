af70c3e14a3a295011ce59676cc59c5c4750ba68
jureYGR <100045612+jureYGR@users.noreply.github.com>
2022-06-03 17:42:54 +0200

    eventredisgn

--- apps/kampakam/src/app/event/event.component.ts
+++ apps/kampakam/src/app/event/event.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
 @Component({
   selector: 'kampakam-event',
   templateUrl: './event.component.html',
@@ -9,9 +10,26 @@ import { firstValueFrom } from 'rxjs';
 export class EventComponent implements OnInit {
   data: any;
 
-  constructor(private http: HttpClient) {}
+  bigDisplay: boolean;
+  layout: string;
+  constructor(
+    private http: HttpClient,
+    private breakpointObserver: BreakpointObserver
+  ) {}
 
   async ngOnInit() {
+    this.breakpointObserver
+      .observe(['(max-width:992px)'])
+      .subscribe((res: BreakpointState) => {
+        this.bigDisplay = !res.matches;
+        if (this.bigDisplay === true) {
+          this.layout = 'grid';
+        } else {
+          this.layout = 'list';
+        }
+        console.log(this.layout);
+      });
+
     this.data = await firstValueFrom(this.http.get('/api/event/getAll'));
   }
 }
